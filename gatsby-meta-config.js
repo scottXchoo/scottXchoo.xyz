bf4c404bfbdcddd94a9b4d1971680fc5a44bc9de
scottXchoo <ckh0601@cau.ac.kr>
2023-10-06 19:29:21 +0900

    chore : 기존 내용 제거

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -47,8 +47,35 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '21.09 - 22.03',
+        activity: '대학생 단기임대 플랫폼 창업 도전',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        date: '22.03 - 22.08',
+        activity: '멋쟁이사자처럼 10기 프론트엔드 파트',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        date: '22.05 - 23.07',
+        activity: '블록체인 스타트업 A41 근무',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        date: '22.06 - 23.05',
+        activity: '블록체인 학회 CURG 학회장',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
@@ -76,9 +103,20 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: 'Supernova',
+        description: 'Cosmos 블록체인의 리퀴드 스테이킹 프로토콜 DeFi 플랫폼',
+        techStack: ['gatsby', 'react'],
+        thumbnailUrl: 'blog.png',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        title: 'COSMonaut',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
+          'Cosmos 블록체인의 컨트랙트 언어인 CosmWasm을 개발자들이 쉽게 배울 수 있도록 돕는 웹사이트',
         techStack: ['gatsby', 'react'],
         thumbnailUrl: 'blog.png',
         links: {
