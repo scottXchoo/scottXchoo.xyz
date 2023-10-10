49cfc01b60d9ddc9a12f8e8456bcd3e565ae72c8
scottXchoo <ckh0601@cau.ac.kr>
2023-10-10 16:15:50 +0900

    feat : PostCard의 Subtitle 짧게 보여주기
    - @가 나오면 잘라주는 유틸 함수 만듦

--- src/components/post-card/index.js
+++ src/components/post-card/index.js
@@ -2,13 +2,21 @@ import { Link } from 'gatsby';
 import React from 'react';
 import './style.scss';
 
+function Substring({ excerpt }) {
+  const index = excerpt.indexOf('@');
+  const substring = index !== -1 ? excerpt.substring(0, index) : excerpt; // '@' 문자가 있다면, 그 전까지의 문자열을 가져오고, 없다면 전체 문자열을 가져옵니다.
+
+  return substring;
+}
+
 function PostCard({ post }) {
   const { id, slug, title, excerpt, date, categories } = post;
   return (
     <div className="post-card-wrapper">
       <Link className="post-card" key={id} to={slug}>
         <div className="title">{title}</div>
-        <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
+        <p className="description">{Substring({ excerpt })}</p>
+        {/* <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} /> */}
         <div className="info">
           <div className="date">{date}</div>
           <div className="categories">
